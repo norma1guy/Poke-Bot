a94506ea614c1a7db31092cb742a075e086f341e
norma1guy <anujk123@hotmail.com>
2026-04-20 18:11:04 +0200

    .gitignore updated

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-tensor = torch.rand(3,4)
-if torch.cuda.is_available():
-    tensor = tensor.to('cuda')
-    print(tensor.device)
-
--- tester.lua
+++
@@ -1,9 +0,0 @@
-from 
-mmap = require('myshm')
-shm = mmap.create_shm()
-
-while true do
-    shm:write('Hello Python!')
-    print(shm:read())
-
-end
